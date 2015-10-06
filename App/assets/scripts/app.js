e03f1e9359aa605beafb7be0cf7f231b303d0bd0
Jordan Delcros <contact@jordan-delcros.com>
2015-10-06 16:24:33 +0200

    Progress on App

--- App/assets/scripts/app.js
+++ App/assets/scripts/app.js
@@ -7,12 +7,30 @@
 	document.addEventListener("DOMContentLoaded", function( event ){
 
 		var quitButton = document.querySelector("#quit");
+		var minimizeButton = document.querySelector("#minimize");
+		var fullscreenButton = document.querySelector("#fullscreen");
 
 		quitButton.addEventListener("click", function( event ){
 
 			event.preventDefault();
 
 			hyper.close();
+
+		}, false);
+
+		minimizeButton.addEventListener("click", function( event ){
+
+			event.preventDefault();
+
+			hyper.minimize();
+
+		}, false);
+
+		fullscreenButton.addEventListener("click", function( event ){
+
+			event.preventDefault();
+
+			hyper.toggleFullscreen();
 
 		}, false);
 
