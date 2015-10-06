1e959219b390e1e3690538d42f4ba2661a02d58f
Jordan Delcros <contact@jordan-delcros.com>
2015-10-06 03:38:02 +0200

    Progress on App

--- App/assets/scripts/app.js
+++ App/assets/scripts/app.js
@@ -1,5 +1,21 @@
 (function( hyper, window, document ){
 
-	console.log(window, hyper);
+	console.log(hyper);
+
+	hyper.showDevTools();
+
+	document.addEventListener("DOMContentLoaded", function( event ){
+
+		var quitButton = document.querySelector("#quit");
+
+		quitButton.addEventListener("click", function( event ){
+
+			event.preventDefault();
+
+			hyper.close();
+
+		}, false);
+
+	}, false);
 
 })((require("nw.gui").Window.get()), window, document);
