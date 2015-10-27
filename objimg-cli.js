0667f4474d54c18f1a13e2b7eee2b98c23e14f58
Jordan Delcros <contact@jordan-delcros.com>
2015-10-27 15:25:16 +0100

    Huge progress on CLI

--- objimg-cli.js
+++ objimg-cli.js
@@ -1,22 +1,75 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 
+var package = require("./package.json");
+var program = require("commander");
 var FileSystem = require("fs");
 var PNG = require("node-png").PNG;
+var OptiPNG = require("optipng");
 
-var arguments = process.argv.slice(2);
+program
+	.version(package.version)
+	.option("-o, --output", "Select output path and name")
+	.option("-c, --compress [quality]", "Optimize image size with OptiPNG", /^[0-9]{1}$/, parseInt)
+	.parse(process.argv);
 
-var input = arguments[0];
+console.log(program.compress);
 
-console.log("OBJImg: ", arguments);
+// var arguments = process.argv.slice(2);
 
-FileSystem.readFile(input, "utf-8", function( error, content ){
+// var actions = {
+// 	input: null,
+// 	output: null,
+// 	compress: {
+// 		active: false,
+// 		quality: 1
+// 	}
+// };
 
-	if( error ){
+// for( var argument = 0, length = arguments.length; argument < length; argument++ ){
 
-		throw error;
+// 	var value = arguments[argument];
 
-	};
+// 	if( value == "-i" || value == "--input" ){
 
-	console.log(content);
+// 		actions.input = arguments[++argument];
 
-});
+// 	}
+// 	else if( value == "-o" || value == "--output" ){
+
+// 		actions.output = arguments[++argument];
+
+// 	}
+// 	else if( value == "-c" || value == "--compress" ){
+
+// 		actions.compress.active = true;
+
+// 		if( /^[0-9]{1}$/.test(arguments[argument + 1]) ){
+
+// 			actions.compress.quality = arguments[++argument];
+
+// 		};
+
+// 	}
+// 	else {
+
+// 		throw new Error("Unsuported parameter '" + value + "'.");
+
+// 	};
+
+// };
+
+// console.log(actions);
+
+// var input = arguments[0];
+
+// FileSystem.readFile(input, "utf-8", function( error, content ){
+
+// 	if( error ){
+
+// 		throw error;
+
+// 	};
+
+// 	// console.log(content);
+
+// });
