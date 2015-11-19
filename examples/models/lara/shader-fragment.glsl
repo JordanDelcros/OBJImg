25024f80a1f46ad38c3c6f8ecf38ac7dfea21073
JordanDelcros <contact@jordan-delcros.com>
2015-11-19 23:05:32 +0100

    fix shader redefining errors

--- examples/models/lara/shader-fragment.glsl
+++ examples/models/lara/shader-fragment.glsl
@@ -1,13 +1,3 @@
-precision highp float;
-precision highp int;
-#define SHADER_NAME ShaderMaterial
-#define MAX_DIR_LIGHTS 0
-#define MAX_POINT_LIGHTS 1
-#define MAX_SPOT_LIGHTS 0
-#define MAX_HEMI_LIGHTS 1
-#define MAX_SHADOWS 0
-#define GAMMA_FACTOR 2
-
 #define PHONG
 uniform vec3 diffuse;
 uniform vec3 emissive;
